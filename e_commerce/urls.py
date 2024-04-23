e24ee26f32f13284cc0cf034f19e3dd60edc0bb1
guilherme-tierno <guilherme.tierno1412@gmail.com>
2024-04-23 15:53:14 -0300

    handle MultipleObjectsReturned except in get_object method in ProductDetailSlugView class in src/products/views.py

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -8,6 +8,7 @@ from products.views import (ProductListView,
                             product_list_view, 
                             ProductDetailView, 
                             product_detail_view,
+                            ProductDetailSlugView,
                             ProductFeaturedListView,
                             ProductFeaturedDetailView)
 
@@ -25,6 +26,7 @@ urlpatterns = [
         path('products-fbv/', product_list_view), 
         path('products/<int:pk>', ProductDetailView.as_view()),
         path('products-fbv/<int:pk>', product_detail_view), 
+        path('products/<slug:slug>/', ProductDetailSlugView.as_view()),
 	path('admin/', admin.site.urls),
         ]
 
--- products/models.py
+++ products/models.py
@@ -29,7 +29,7 @@ class ProductManager(models.Manager):
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField()
+    slug        = models.SlugField(unique=True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.FileField(upload_to = 'products/', null = True, blank = True)
@@ -42,7 +42,7 @@ class Product(models.Model): #product_category
     #python 3
     def __str__(self):
         return self.title
-        
+
     #python 2
     def __unicode__(self):
         return self.title
