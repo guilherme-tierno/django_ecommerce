c0ab1415370bffb621edcb7a9c89c12bf56a8b07
guilherme-tierno <guilherme.tierno1412@gmail.com>
2024-05-20 16:59:32 -0300

    search

--- products/models.py
+++ products/models.py
@@ -1,14 +1,23 @@
+from django.db.models import Q
 from django.db import models
 from .utils import unique_slug_generator
 from django.db.models.signals import pre_save
 from django.urls import reverse
+
 #Custom queryset
 class ProductQuerySet(models.query.QuerySet):
+    
     def active(self):
         return self.filter(active = True)
 
     def featured(self):
         return self.filter(featured = True, active = True)
+
+    def search(self, query):
+        lookups = (Q(title__contains = query) | 
+                   Q(description__contains = query) | 
+                   Q(price__contains = query))
+        return self.filter(lookups).distinct()
 
 class ProductManager(models.Manager):
     
@@ -19,7 +28,7 @@ class ProductManager(models.Manager):
         return self.get_queryset().active()
 
     def featured(self):
-        #return self.get_queryset().filter(featured = True)
+        #self.get_queryset().filter(featured = True)
         return self.get_queryset().featured()
 
     def get_by_id(self, id):
@@ -28,10 +37,13 @@ class ProductManager(models.Manager):
             return qs.first()
         return None
 
+    def search(self, query):
+        return self.get_queryset().active().search(query)
+
 # Create your models here.
 class Product(models.Model): #product_category
     title       = models.CharField(max_length=120)
-    slug        = models.SlugField(blank = True, unique=True)
+    slug        = models.SlugField(blank = True, unique = True)
     description = models.TextField()
     price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
     image       = models.FileField(upload_to = 'products/', null = True, blank = True)
@@ -49,11 +61,11 @@ class Product(models.Model): #product_category
     #python 3
     def __str__(self):
         return self.title
-
+        
     #python 2
     def __unicode__(self):
         return self.title
-    
+
 def product_pre_save_receiver(sender, instance, *args, **kwargs):
     if not instance.slug:
         instance.slug = unique_slug_generator(instance)
